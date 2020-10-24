90a5b4d8a25e0a8dfca609905f7c9d95c78beb43
MussoNero <mussonero@gmail.com>
2020-10-25 00:09:13 +0200

    Making the example more interesting

--- example.cpp
+++ example.cpp
@@ -11,14 +11,34 @@
 using namespace binance;
 using namespace std;
 
-map<long, map<string, double> > klinesCache;
+void print_depthCache(map < string, map <double,double> >  depthCache) {
 
-static void print_klinesCache()
+	map < string, map <double,double> >::iterator it_i;
+
+	for ( it_i = depthCache.begin() ; it_i != depthCache.end() ; it_i++ ) {
+
+		string bid_or_ask = (*it_i).first ;
+		cout << bid_or_ask << endl ;
+		cout << "Price             Qty" << endl ;
+
+		map <double,double>::reverse_iterator it_j;
+
+		cout << "==================================" << endl;
+		for ( it_j = depthCache[bid_or_ask].rbegin() ; it_j != depthCache[bid_or_ask].rend() ; it_j++ ) {
+
+			double price = (*it_j).first;
+			double qty   = (*it_j).second;
+			printf("%.08f          %.08f\n", price, qty );
+		}
+		cout << "==================================" << endl;
+	}
+}
+
+void print_klinesCache(map<long, map<string, double> > klinesCache)
 {
-	cout << "==================================" << endl;
-	
+
 	for (map<long, map<string, double> >::iterator it_i = klinesCache.begin();
-		it_i != klinesCache.end(); it_i++)
+		 it_i != klinesCache.end(); it_i++)
 	{
 		long start_of_candle = (*it_i).first;
 		map <string,double> candle_obj 	= (*it_i).second;
@@ -31,21 +51,55 @@ static void print_klinesCache()
 		cout << "v:" << candle_obj["v"] ;
 		cout << " " << endl;
 	}
+	cout << "==================================" << endl;
 }
 
-static int ws_klines_onData(Json::Value& json_result)
-{	
-	long start_of_candle = json_result["k"]["t"].asInt64();
-	klinesCache[start_of_candle]["o"] = atof(json_result["k"]["o"].asString().c_str());
-	klinesCache[start_of_candle]["h"] = atof(json_result["k"]["h"].asString().c_str());
-	klinesCache[start_of_candle]["l"] = atof(json_result["k"]["l"].asString().c_str());
-	klinesCache[start_of_candle]["c"] = atof(json_result["k"]["c"].asString().c_str());
-	klinesCache[start_of_candle]["v"] = atof(json_result["k"]["v"].asString().c_str());
-	
-	print_klinesCache();
-	
+int ws_klines_onData(Json::Value& json_result)
+{
+	map<long, map<string, double> > klinesCache;
+	map < string, map <double,double> >  depthCache;
+
+	if (json_result["lastUpdateId"].isNumeric() && json_result["bids"].isArray() &&  json_result["asks"].isArray()) {
+		int i;
+
+		for ( i = 0 ; i < json_result["bids"].size() ; i++ ) {
+			double price = atof( json_result["bids"][i][0].asString().c_str());
+			double qty 	 = atof( json_result["bids"][i][1].asString().c_str());
+			if ( qty == 0.0 ) {
+				depthCache["bids"].erase(price);
+			} else {
+				depthCache["bids"][price] = qty;
+			}
+		}
+		for ( i = 0 ; i < json_result["asks"].size() ; i++ ) {
+			double price = atof( json_result["asks"][i][0].asString().c_str());
+			double qty 	 = atof( json_result["asks"][i][1].asString().c_str());
+			if ( qty == 0.0 ) {
+				depthCache["asks"].erase(price);
+			} else {
+				depthCache["asks"][price] = qty;
+			}
+		}
+		print_depthCache(depthCache);
+	}else if(json_result["e"].asString() == "kline"){
+		long start_of_candle = json_result["k"]["t"].asInt64();
+		klinesCache[start_of_candle]["o"] = atof(json_result["k"]["o"].asString().c_str());
+		klinesCache[start_of_candle]["h"] = atof(json_result["k"]["h"].asString().c_str());
+		klinesCache[start_of_candle]["l"] = atof(json_result["k"]["l"].asString().c_str());
+		klinesCache[start_of_candle]["c"] = atof(json_result["k"]["c"].asString().c_str());
+		klinesCache[start_of_candle]["v"] = atof(json_result["k"]["v"].asString().c_str());
+
+		cout << "=============="<< json_result["s"].asString() <<"====================" << endl;
+		print_klinesCache(klinesCache);
+	}else if(json_result["e"].asString() == "24hrMiniTicker"){
+		cout << "=====24hrMiniTicker\\Begin========="<< json_result["s"].asString() <<"=======24hrMiniTicker\\Begin" << endl;
+		cout << json_result << endl;
+		cout << "=====24hrMiniTicker\\End========="<< json_result["s"].asString() <<"=======24hrMiniTicker\\End====" << endl;
+	}
+
 	return 0;
 }
+
 
 int main()
 {
@@ -53,39 +107,67 @@ int main()
 	Logger::set_debug_logfp(stderr);
 
 	Json::Value result;
-	double serverTime;
 
 	Server server;
-	
+
 	Market market(server);
-		
+
 	// Klines / CandleStick
-#if 1
-	BINANCE_ERR_CHECK(market.getKlines(result, "BTCUSDT", "1h", 0, 0, 10));
- 	for (Json::Value::ArrayIndex i = 0 ; i < result.size() ; i++)
- 	{
- 		long start_of_candle = result[i][0].asInt64();
- 		klinesCache[start_of_candle]["o"] = atof(result[i][1].asString().c_str());
- 		klinesCache[start_of_candle]["h"] = atof(result[i][2].asString().c_str());
- 		klinesCache[start_of_candle]["l"] = atof(result[i][3].asString().c_str());
- 		klinesCache[start_of_candle]["c"] = atof(result[i][4].asString().c_str());
- 		klinesCache[start_of_candle]["v"] = atof(result[i][5].asString().c_str());
- 	}
+	BINANCE_ERR_CHECK(market.getKlines(result, "BNBUSDT", "1w", 0, 0, 10));
+	map<long, map<string, double> > klinesCache;
+	for (Json::Value::ArrayIndex i = 0 ; i < result.size() ; i++)
+	{
 
-    print_klinesCache();
-#else
-    BINANCE_ERR_CHECK(market.getLastFundingRate(result, "BTCUSDT"));
-    std::cout << stod(result["lastFundingRate"].asString()) << endl;
+		long start_of_candle = result[i][0].asInt64();
+		klinesCache[start_of_candle]["o"] = atof(result[i][1].asString().c_str());
+		klinesCache[start_of_candle]["h"] = atof(result[i][2].asString().c_str());
+		klinesCache[start_of_candle]["l"] = atof(result[i][3].asString().c_str());
+		klinesCache[start_of_candle]["c"] = atof(result[i][4].asString().c_str());
+		klinesCache[start_of_candle]["v"] = atof(result[i][5].asString().c_str());
+	}
 
-    BINANCE_ERR_CHECK(market.getServerTime(result));
-    std::cout << stod(result["serverTime"].asString()) << endl;
-#endif
+	print_klinesCache(klinesCache);
 
- 	// Klines/Candlestick update via websocket
- 	Websocket::init();
- 	Websocket::connect_endpoint(ws_klines_onData, "/ws/btcusdt@kline_1m"); 
-	Websocket::enter_event_loop();
-	
+	while (true){
+		Websocket::init();
+		sleep (1);
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/poebtc@depth20@1000ms");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/batbtc@depth20@1000ms");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/xtzusdt@depth20@1000ms");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/bnbusdt@depth20@1000ms");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/algobnb@depth20@1000ms");
+
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/poebtc@kline_1m");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/batbtc@kline_1m");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/xtzusdt@kline_1m");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/bnbusdt@kline_1m");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/algobnb@kline_1m");
+
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/xtzbtc@depth20@1000ms");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/xtzbtc@miniTicker");
+
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/xrpbtc@depth20@1000ms");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/xrpbtc@miniTicker");
+
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/eosusdt@depth20@1000ms");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/eosusdt@miniTicker");
+
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/algousdt@depth20@1000ms");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/algousdt@miniTicker");
+
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/xrpbnb@depth20@1000ms");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/xrpbnb@miniTicker");
+
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/ethusdt@depth20@1000ms");
+		Websocket::connect_endpoint(ws_klines_onData, "/ws/ethusdt@miniTicker");
+
+
+		Websocket::enter_event_loop();
+		cout << "error exiting enter_event_loop and we will try again after 5sec" << endl;
+		sleep(5);
+	}
+
+
 	return 0;
 }
 
